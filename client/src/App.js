9e23ba7ae5cc54ab954faf1671dc79058ef51676
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-04-29 16:37:40 -0500

    Clean project and form structure added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Usuario</label>
+            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <button type="submit" className="btn btn-primary" onClick={imprimirResultados} >Enviar</button>
+        </form>
+      </div>
+    </>
   );
 }
 
+//Imprimir en consola los datos enviados del formulario
+const imprimirResultados = (e) => {
+  e.preventDefault();
+  console.log(document.getElementById('exampleInputEmail1').value);
+  console.log(document.getElementById('exampleInputPassword1').value);
+}
 export default App;
